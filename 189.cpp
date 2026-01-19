c55ccc43989cee607faeabbf9f290ecf5683a981
Harshita1204 <harshitak1204@gmail.com>
2026-01-19 22:21:02 +0530

    Clarify purpose of temporary vector in rotate function
    Clarify the purpose of the temporary vector used in the rotate function to prevent data overwriting during the rotation process.

--- 189.cpp
+++ 189.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        vector<int> temp(nums.size());
+        vector<int> temp(nums.size()); // we are using temp because if we use the same array to do this process , it will overwrite it! so we are using temp to store it for temporary purpose
         for(int i=0;i<nums.size();i++){
             temp[(i+k)%nums.size()]=nums[i];
         }
