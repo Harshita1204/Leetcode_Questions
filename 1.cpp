85b484fa5cd5ae3e0a92ca99c952b6cf50697891
Harshita1204 <harshitak1204@gmail.com>
2026-01-29 22:44:45 +0530

    Create 1.cpp

---
+++ 1.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n= nums.size(); // stores the sizr of an array
+        for(int i=0;i<n;i++){ // loop for the first number
+            for(int j=i+1;j<n;j++){ // loop for second number after i
+                if(nums[i]+nums[j]==target){   // checks if sum of two numbers equals to the target
+                    return {i,j};   // return their indices
+                }
+            }
+        }
+        return {};
+    }
+};
