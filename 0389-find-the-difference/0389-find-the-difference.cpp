2970dab5e605a545f6b5927244ea08eff1883568
Harshitapreet kaur <harshitak1204@gmail.com>
2026-06-24 14:28:00 +0530

    Time: 0 ms (100%), Space: 9.1 MB (77.64%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        vector<int> f(26,0);
+        for(char ch: s){
+            f[ch-'a']++;
+        }
+        for(char ch:t){
+            f[ch-'a']--;
+            if(f[ch-'a']<0){
+                return ch;
+            }
+        }
+        return ' ';
+    }
+};
