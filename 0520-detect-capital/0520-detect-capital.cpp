2b1197b8322ac653cb72a73dfa1b41e1b99a56e9
Harshitapreet kaur <harshitak1204@gmail.com>
2026-06-29 10:40:47 +0530

    Time: 0 ms (100%), Space: 8.2 MB (51.23%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.cpp
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -7,16 +7,14 @@ public:
                 capitalCount++;
         }
     
-            if(capitalCount == word.size())
+            if(capitalCount == word.size()) // all letter are capital 
                 return true;
             
-            if(capitalCount == 0)
+            if(capitalCount == 0) // no letter is capital ie every letter is in lowercase
                 return true;
             
-            if(capitalCount == 1 && isupper(word[0]))
+            if(capitalCount == 1 && isupper(word[0])) // only first letter is capital rest all are small
                 return true;
-            
-            //return false;
         
         return false;
     }
